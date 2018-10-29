c6c37d75fe3bf7b5811094b5e61d106d8df989b9
Elizabeth Malygina <elizabeth.malygina@gmail.com>
2018-10-30 00:28:46 +0200

    test

--- src/js/parts/modal.js
+++ src/js/parts/modal.js
@@ -2,19 +2,21 @@ function modal() {
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
+   
+        more.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+        });
 
-    more.addEventListener('click', function () {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-    });
-
-    close.addEventListener('click', () => {
-        overlay.style.display = 'none';
-        this.classList.remove('more-splash');
-        document.body.style.overflow = '';
-    });
+        close.addEventListener('click', () => {
+            overlay.style.display = 'none';
+            this.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        });
+    
 
     let content = document.querySelector('.content');
+
 
     content.addEventListener('click', (event) => {
         if (event.target && event.target.classList.contains('description-btn')) {
